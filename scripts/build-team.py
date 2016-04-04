cc988ceaf5029272aa001c1d64b34c0486bd3c7e
Nathan Scott <nathans@redhat.com>
2016-04-04 10:59:04 +1000

    Update build-team.py to list maintainers and filter git-log further

--- scripts/build-team.py
+++ scripts/build-team.py
@@ -23,10 +23,18 @@ def get_authors_haml_columns(authors):
         s2 += '%s%%li %s\n' % (indent, author)
     return (s1, s2)
 
-ret = subprocess.check_output(["sh", "-c", "git -C %s log --pretty=format:'%%an' | egrep -v 'pcpqa|unknown|EC2' | sort | uniq | sort" % sys.argv[1]])
+# filter out a handful of git history author botches
+ret = subprocess.check_output(["sh", "-c", "git -C %s log --pretty=format:'%%an' | egrep -v 'pcpqa|unknown|EC2|cessna|buffalo.edu|test-account-0' | sort | uniq" % sys.argv[1]])
+everyone = ret.split('\n')
 
-authors = ret.split('\n')
-(col1, col2) = get_authors_haml_columns(authors)
+# split apart the maintainers and contributors groups
+core = ('Dave Brolley', 'Ken McDonell', 'Lukas Berk', 'Mark Goodwin', 'Nathan Scott')
+col0 = ''
+for person in core:
+    everyone.remove(person)
+    col0 += ' ' * 20 + '%li ' + person + '\n'
+
+(col1, col2) = get_authors_haml_columns(everyone)
 
 template = '''
 !!! html
@@ -46,8 +54,20 @@ template = '''
             %section{:class => 'row__colspaced'}
               %div{:class => 'colspan12-8 push12-2 colspan8-8 colspan6-6 colspan2-1 as-grid with-gutter'}
                 %div{:class => 'col__module--cta'}
-                  %h2 Team
-                  %p Performance Co-Pilot is brought to you by the following people.
+                  %h2 Maintainers
+                  %p Performance Co-Pilot releases are coordinated by the core team.
+                  %ul
+__COLUMN0__
+
+        %div{:class => 'row-parent'}
+          %div{:class => 'row'}
+            %section{:class => 'row__colspaced'}
+              %div{:class => 'logo colspan12-12 colspan8-8 colspan6-6 colspan2-2 as-grid with-gutter'}
+            %section{:class => 'row__colspaced'}
+              %div{:class => 'colspan12-8 push12-2 colspan8-8 colspan6-6 colspan2-1 as-grid with-gutter'}
+                %div{:class => 'col__module--cta'}
+                  %h2 Contibutors
+                  %p Releases bring together the efforts of a wide developer community.
 
         %div{:class => 'row-parent'}
           %div{:class => 'row'}
@@ -65,6 +85,7 @@ __COLUMN2__
     = Haml::Engine.new(File.read("assets/haml-includes/footer.haml")).render
 '''
 
-t1 = template.replace('__COLUMN1__', col1)
+t0 = template.replace('__COLUMN0__', col0)
+t1 = t0.replace('__COLUMN1__', col1)
 t2 = t1.replace('__COLUMN2__', col2)
 print(t2)
